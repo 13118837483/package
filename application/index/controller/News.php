0b10b50b532a287775b0aa7d7517d176d77cf766
13118837483 <824918353@qq.com>
2022-10-17 05:06:58 +0800

    dd

---
+++ application/index/controller/News.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace app\index\controller;
+// USE think\Db;
+use app\index\model\Test as testModel;
+use app\index\model\Product as ProductModel;
+use think\cache\driver\Redis;
+// use think\Cache\
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use Predis\Client;
+
+class News extends Command
+{
+    // public function index()
+    // {
+    //     $data = ProductModel::select();
+    //     // ProductModel::create([
+    //     //     'name' => '测试数据库名称',
+    //     //     'round' => '1212'
+    //     // ]);
+    //     // $data = ProductModel::select();
+    //     // foreach($data as &$v){
+    //     //     $v = $v->toArray();
+    //     // }
+
+    //     // // halt($data);
+    //     // return json($data);
+    //     // $addArr = [
+    //     //     'user_name' => '测试数据库名称',
+    //     // ];
+    // for ($i=0; $i < 10; $i++) { 
+    //     ProductModel::create([
+    //         'user_name' => '测试数据库名称',
+    //         'name' => '测试数据库名称',
+    //         'avc' => '测试数据库名称',
+    //         'adsf' => '测试数据库名称',
+    //         'wer' => '测试数据库名称',
+    //         'rtyrt' => '测试数据库名称',
+    //         'ghj' => '测试数据库名称',
+    //         'password' => '测试数据库名称',
+    //         'ertertr' => '测试数据库名称',
+    //         'vcg' => '测试数据库名称',
+    //         'zxc' => '测试数据库名称',
+    //         'hgf' => '测试数据库名称',
+    //         'sdf' => '测试数据库名称',
+    //         'werwer' => '测试数据库名称',
+    //         'werweeer' => '测试数据库名称',
+    //         'weeeeer' => '测试数据库名称',
+    //         'werwe' => '测试数据库名称',
+
+    //     ]);
+    // }
+    // return true;
+    //     // $redis = new Redis();
+    //     // $redis->set('huaneddg','fdsfsdfsdfswrwerwerwerwerdfsd');
+    //     // $data = $redis->get('huaneddg');
+    //     // halt($data);
+    //     // $model = new testModel();
+    //     // halt('dd');
+    //     // $data = $model::where(['id' => 1])->find();
+    //     // return json($data);
+    // }
+    protected function configure()
+    {
+        $this->setName('product')->setDescription('Here is the remark ');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        for ($i = 0; $i < 1000000; $i++) {
+            dump($i);
+            
+            ProductModel::create([
+                'user_name' => '测试数据库名称',
+                'name' => '测试数据库名称',
+                'avc' => '测试数据库名称',
+                'adsf' => '测试数据库名称',
+                'wer' => '测试数据库名称',
+                'rtyrt' => '测试数据库名称',
+                'ghj' => '测试数据库名称',
+                'password' => '测试数据库名称',
+                'ertertr' => '测试数据库名称',
+                'vcg' => '测试数据库名称',
+                'zxc' => '测试数据库名称',
+                'hgf' => '测试数据库名称',
+                'sdf' => '测试数据库名称',
+                'werwer' => '测试数据库名称',
+                'werweeer' => '测试数据库名称',
+                'weeeeer' => '测试数据库名称',
+                'werwe' => '测试数据库名称',
+
+            ]);
+        }
+    }
+}
---
+++ application/index/controller/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\index\controller;
+// USE think\Db;
+use app\index\model\Test as testModel;
+use app\index\model\Product as ProductModel;
+use think\cache\driver\Redis;
+// use think\Cache\
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use Predis\Client;
+use think\console\command\make\Controller;
+
+class Product extends Controller
+{
+
+   public function index()
+   {
+      $get = input('get.');
+      //    halt($get);
+      $data = testModel::where(['id' => ['<', 1000]])->select();
+      for ($i = 0; $i < 1000; $i++) {
+         $name = $this->getChar(3);
+         $passwordName = $this->getChar(2);
+
+         // dump($passwordName);
+         testModel::create([
+            'user_name' => $name,
+            'password' => $passwordName,
+            'age' => $i,
+         ]);
+      }
+      return json($data);
+   }
+
+   public function getChar($num)  // $num为生成汉字的数量
+   {
+      $b = '';
+      for ($i = 0; $i < $num; $i++) {
+         // 使用chr()函数拼接双字节汉字，前一个chr()为高位字节，后一个为低位字节
+         $a = chr(mt_rand(0xB0, 0xD0)) . chr(mt_rand(0xA1, 0xF0));
+         // 转码
+         $b .= iconv('GB2312', 'UTF-8', $a);
+      }
+      if($num != 3) $b = $b . '465165456';
+      return $b;
+   }
+}
